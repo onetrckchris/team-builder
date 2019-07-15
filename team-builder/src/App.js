e98a2850cfd913ad014c80b43450783861388e92
onetrckchris <onetrckchris@gmail.com>
2019-07-15 15:32:50 -0700

    now able to add team members and populate the form with the member you want to edit. Still unable to edit correctly.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -6,11 +6,56 @@ import Form from './components/Form';
 
 function App() {
   const [teamState, setTeamState] = useState(team);
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    role: ''
+  });
+  const [memberToEdit, setMemberToEdit] = useState();
+
+  const changeHandler = event => {
+    if(memberToEdit) {
+      setMemberToEdit({...memberToEdit, [event.target.name]: event.target.value})
+    } else {
+      setFormState({...formState, [event.target.name]: event.target.value});
+    }
+  }
+
+  const addMember = event => {
+    event.preventDefault();
+    setTeamState([...teamState, formState]);
+    setFormState({
+      name: '',
+      email: '',
+      role: ''
+    })
+  }
+
+  const editMember = event => {
+    event.preventDefault();
+
+  }
+
+  const fillEditForm = member => {
+    setMemberToEdit(member);
+  }
 
   return (
     <div className="App">
+      <Form 
+        changeHandler={changeHandler} 
+        formState={formState} 
+        addMember={addMember} 
+        editMember={editMember}
+        memberToEdit={memberToEdit}
+      />
       {teamState.map(member => (
-        <h1 key={member.name}>{member.name}</h1>
+        <div key={member.name}>
+          <p>{member.name}</p>
+          <p>{member.email}</p>
+          <p>{member.role}</p>
+          <button onClick={() => fillEditForm(member)}>Edit</button>
+        </div>
       ))}
     </div>
   );
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -2,8 +2,27 @@ import React from 'react';
 
 const Form = props => {
     return (
-        <form>
+        <form onSubmit={props.memberToEdit ? props.editMember : props.addMember}>
+            <input 
+                type="text" 
+                name="name" 
+                placeholder="Name" 
+                onChange={props.changeHandler} 
+                value={props.memberToEdit ? props.memberToEdit.name : props.formState.name} />
+            <input 
+                type="email" 
+                name="email" 
+                placeholder="Email" 
+                onChange={props.changeHandler} 
+                value={props.memberToEdit ? props.memberToEdit.email : props.formState.email} />
+            <input 
+                type="text" 
+                name="role" 
+                placeholder="Role" 
+                onChange={props.changeHandler} 
+                value={props.memberToEdit ? props.memberToEdit.role : props.formState.role} />
 
+            <input type="submit" value={props.memberToEdit ? "Edit Member" : "Add Member"} />
         </form>
     )
 }
